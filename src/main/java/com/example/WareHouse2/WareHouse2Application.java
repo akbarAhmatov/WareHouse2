f397378c982655fe0cc173dc2cf902bf459ace23
gammer <145098169+akbarAhmatov@users.noreply.github.com>
2025-11-04 17:48:58 +0500

    firts commit

---
+++ src/main/java/com/example/WareHouse2/WareHouse2Application.java
@@ -0,0 +1,13 @@
+package com.example.WareHouse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouse2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WareHouse2/WareHouse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WareHouse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareHouse2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
